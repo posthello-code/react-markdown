25f66d0c5b074eef6e5c1c8b409ce4919ee5dd21
posthello-code <github@alexjensen.net>
2024-05-14 23:33:19 -0700

    remove unused file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import "./App.css";
-import { NavBar } from "./NavBar";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import { Grommet } from "grommet";
 import Home from "./content/Home";
 import Error from "./content/Error";
 
 function Layout() {
   return (
     <>
-      <Grommet id="grommet" align="center">
-        {/* NavBar component currently hidden */}
-        <NavBar />
-      </Grommet>
       {/* Render the content for current page based on Browser router state below*/}
       <Outlet></Outlet>
     </>
